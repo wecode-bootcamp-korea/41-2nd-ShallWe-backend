98893b2b979833b658e6136186d3fb5d4ea64d43
JongHoB <78012131+JongHoB@users.noreply.github.com>
2023-01-30 21:09:40 +0900

    [ADD] 초기환경 세팅

---
+++ api/middlewares/error.js
@@ -0,0 +1,23 @@
+const asyncErrorHandler = (func) => {
+  return async (req, res, next) => {
+    await func(req, res).catch((error) => next(error));
+  };
+};
+
+const errorHandler = (err, request, response, next) => {
+  console.error(err);
+  return response.status(err.statusCode || 500).json({ message: err.message });
+};
+
+const throwCustomError = (message, statusCode) => {
+  const err = new Error(message);
+  err.statusCode = statusCode;
+
+  throw err;
+};
+
+module.exports = {
+  errorHandler,
+  asyncErrorHandler,
+  throwCustomError,
+};
