c23ae00bb79e2dbf0afc93557acef0c883b9faef
sang woo <tooil1729@gmail.com>
2023-02-07 18:33:55 +0900

    [박상우] Modify : 메인 페이지 및 리스트 페이지 기능 수정

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-// 회원가입, 로그인. 시용자 정보 라우터
 const userRouter = require("./userRoutes");
+const movieRouter = require("./movieRoutes");
 
 const orderRouter = require("./orderRoutes");
 const pickRouter = require("./pickRoutes");
 const refundRouter = require("./refundRoutes");
 const reviewRouter = require("./reviewRoutes");
 const subscriptionRouter = require("./subscriptionRoutes");
-const movieRouter = require("./movieRoutes");
 
 router.use("/users", userRouter.router);
+router.use("/movies", movieRouter.router);
 router.use("/myShallWe/userInfo", userRouter.router);
 router.use("/myShallWe/orders", orderRouter.router);
 router.use("/myShallWe/pick", pickRouter.router);
@@ -19,5 +19,6 @@ router.use("/myShallWe/refunds", refundRouter.router);
 router.use("/myShallWe/reviews", reviewRouter.router);
 router.use("/myShallWe/subscriptions", subscriptionRouter.router);
 router.use("/shallWeMovie", movieRouter.router);
+
 
 module.exports = router;
--- api/services/movieService.js
+++ api/services/movieService.js
@@ -1,10 +1,28 @@
 const movieDao = require("../models/movieDao");
 const { throwCustomError } = require("../middlewares/error");
 
+require("dotenv").config();
+
 const movieDetails = async (movieId) => {
   const result = await movieDao.movieDetails(movieId);
   if (!result.length) throwCustomError("Movie is not found", 404);
   return result;
 };
 
-module.exports = { movieDetails };
+const getMovieCategory = async ( category, page ) => {
+
+  const movieCategory = await movieDao.movieCategory( category, page );
+  
+  if ( !movieCategory ) {
+    const err = new Error("NOT_movieCategory");
+    err.statusCode = 404;
+    throw err;
+  }
+
+  return movieCategory
+};
+
+module.exports = {
+  movieDetails,
+  getMovieCategory,
+};
