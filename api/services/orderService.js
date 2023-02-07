10b51ee7adaaa6820248fb88c316ec6925792102
JongHoB <78012131+JongHoB@users.noreply.github.com>
2023-02-07 13:12:27 +0900

    [ADD] 영화 장바구니 카카오페이 결제 기능 추가

--- api/services/orderService.js
+++ api/services/orderService.js
@@ -4,6 +4,16 @@ const getOrders = async (userId) => {
   return await orderDao.getOrders(userId);
 };
 
+const completeOrders = async (tid, paymentType, pickId) => {
+  const type = Object.freeze({
+    MONEY: 2,
+    CARD: 3,
+  });
+
+  return await orderDao.completeOrders(tid, type[paymentType], pickId);
+};
+
 module.exports = {
   getOrders,
+  completeOrders,
 };
